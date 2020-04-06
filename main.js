53a65789215e62746faacb3a70dffa30dae0e1b5
Simon B <simon.bohlin@gmail.com>
2020-04-06 17:22:20 +0200

    Update main.js
    Wider random char code

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ window.onload = () => {
     open(uniqueUrl);
   }
 
+  const threeChars = () => {
+    var n = (Math.random() * 46656) | 0;
+    return ("000" + n.toString(36)).slice(-3);
+  }
+
   // 6-character alphanumeric sequence
   const generateUID = () => {
-    var firstPart = (Math.random() * 46656) | 0;
-    var secondPart = (Math.random() * 46656) | 0;
-    firstPart = ("000" + firstPart.toString(36)).slice(-3);
-    secondPart = ("000" + secondPart.toString(36)).slice(-3);
-    return firstPart + secondPart;
+    return threeChars() + threeChars() + threeChars();
   }
 }
