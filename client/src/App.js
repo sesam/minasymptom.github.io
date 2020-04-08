39331df71cf3df9d8e28de09096091bf94dcbaaa
Simon Bohlin <simon.bohlin@tretton37.com>
2020-04-08 17:35:11 +0200

    Clean away Start

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Home } from "./components/Home";
-import { Start } from "./components/Start";
 import { Checkup } from "./components/Checkup";
 import { ONE_HOUR } from "@cocheck/api";
 import { COVID19symptoms } from "@cocheck/checks";
 
 function App() {
   const [nextCheckup, setNextCheckup] = React.useState();
-  const [userData, setUserData] = React.useState();
   const [showForm, setShowForm] = React.useState();
-
-  const enableCheckup = React.useCallback(
-    (data) => {
-      setUserData(data);
-      setNextCheckup(new Date() + ONE_HOUR);
-    },
-    [setUserData]
-  );
-
   const saveCheckup = React.useCallback((data) => {
     console.log("save checkup...", data);
     setNextCheckup(new Date() + ONE_HOUR);
@@ -28,8 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {!showForm && <Home onStartForm={() => setShowForm(true)} />}
-        {showForm && !userData && <Start onSave={enableCheckup} />}
-        {showForm && !!userData && (
+        {showForm && (
           <Checkup
             checks={COVID19symptoms}
             nextCheckup={nextCheckup}
